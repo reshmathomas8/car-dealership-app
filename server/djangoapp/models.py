8a34da2fb0bff684ec1766618b081f9f2c3ccbe3
reshmathomas8 <reshmathomas35@gmail.com>
2025-07-22 10:51:23 +0530

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,11 +1,7 @@
-# Uncomment the following imports before adding the Model code
-
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
-# Create your models here.
 # Car's make model
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
